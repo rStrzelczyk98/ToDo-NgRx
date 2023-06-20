41188bf4e2a98150a684cd87ebe51f382edc83af
Robert Strzelczyk <123934951+rStrzelczyk98@users.noreply.github.com>
2023-06-20 13:04:42 +0200

    toDoApp with NgRx (#1)
    Co-authored-by: = <=>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoComponent } from './to-do/to-do.component';
+import { ToDoListComponent } from './to-do/to-do-list/to-do-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ToDoComponent,
+    children: [
+      { path: 'all', component: ToDoListComponent },
+      { path: 'active', component: ToDoListComponent },
+      { path: 'completed', component: ToDoListComponent },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
